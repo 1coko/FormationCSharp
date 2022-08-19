7a5e3a18f60723c6fe267cba5023ed2c9f223f0b
1coko <malek_45@hotmail.fr>
2022-08-19 19:28:00 +0200

    Ajout d'une nouvelle execution qui demande le nom

--- Exercice2bis/Program.cs
+++ Exercice2bis/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string nom;
+Console.WriteLine("Merci de saisir votre nom");
+
+nom = Console.ReadLine();
+Console.WriteLine("Bonjour, mon nom est : " + nom);
